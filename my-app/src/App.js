dfca625e33f4fd901aebf0de400eea276b7487f4
Faizan Ali <faizan0ali158@gmail.com>
2025-08-04 13:50:48 +0500

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello Sania g</p>
+    </div>
+  );
+}
+
+export default App;
